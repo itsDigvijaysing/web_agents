57a11006c7ad97510ff9fd14f54fe872af8fdddc
itsdigvijaysing <itsdigvijaysing@gmail.com>
2026-07-13 01:23:50 +0530

    Cleanup of Repo

--- web_agent/skills/__init__.py
+++
@@ -1,4 +0,0 @@
-from web_agent.skills.service import SkillService
-from web_agent.skills.views import MissingCookieException
-
-__all__ = ['SkillService', 'MissingCookieException']
--- web_agent/sync/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Cloud sync module for web agent."""
-
-from web_agent.sync.auth import CloudAuthConfig, DeviceAuthClient
-from web_agent.sync.service import CloudSync
-
-__all__ = ['CloudAuthConfig', 'DeviceAuthClient', 'CloudSync']
